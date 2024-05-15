5004e56b40d934a1402dc6af728acc3c72e65d7b
amit <amit.sakhare37@gmail.com>
2024-05-15 17:16:49 +0530

    Day-1 Completed

---
+++ Day-1 Create Database .sql
@@ -0,0 +1,11 @@
+/*Syntax of Createing Database
+CREATE DATABASE DATABASE_name;*/
+
+#Check Database already you have created
+show DATABASES;
+
+#How to Crate Student name Database
+CREATE DATABASE student;
+
+#How to Drop database
+DROP DATABASE student;
